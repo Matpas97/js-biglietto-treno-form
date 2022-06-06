a15c542e5e6d1428087eb8db0a4e3624caf24727
Matpas97 <passarellim22@gmail.com>
2022-06-06 16:20:07 +0200

    Js

--- js/script.js
+++ js/script.js
@@ -5,4 +5,33 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 
-// Chiedo all'utente la sua eta
+// Chiedo all'utente la sua eta e Km
+
+const generateButton = document.getElementById('generate-ticket');
+generateButton.addEventListener('clik',
+function() {
+    const userName = document.getElementById('user-name').value;
+    const userKm = parseInt( document.getElementById('user-km').value);
+    const userAge = document.getElementById('user-age').value;
+
+    // Calcolo il prezzo totale del viaggio
+    const price = userKm * 0.21;
+
+    let reduction = 0;
+
+    if (userAge === 'minorenne') {
+        reduction = price * 20 / 100;
+     } else if (userAge === 'over') {
+         reduction = price * 40 / 100;
+     }
+    const finalPrice = price - reduction;
+
+    document.getElementById('ticket-name').value;
+    document
+
+}
+   
+
+
+
+)
